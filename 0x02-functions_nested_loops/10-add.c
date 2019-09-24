526da617e7d5a0c9cbbdfac69bf7c252ef587c34
Michael <maicol.asus@gmail.com>
2019-09-24 20:47:16 +0000

    task 10 for C project

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add - sum two numbers and return.
+ * @n1: Number one
+ * @n2: Number two
+ * Return: sum of two numbers.
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
