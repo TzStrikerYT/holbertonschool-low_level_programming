d2e756707aaf64eafbcc26adeba2b6c5822fbf3e
Michael <maicol.asus@gmail.com>
2019-09-24 19:15:57 +0000

    task 8 for c project

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * main - count hour
+ *
+ * Return: Always 0.
+ */
+void jack_bauer(void)
+{
+
+	int i;
+	int j;
+	int h1;
+	int h2;
+        int m1;
+	int m2;
+
+	for(i = 0; i < 24; i++)
+	{
+		for(j = 0; j < 60; j++)
+		{
+			_putchar('0' + i / 10);
+			_putchar('0' + i % 10);
+			_putchar(':');
+			_putchar('0' + j / 10);
+			_putchar('0' + j % 10);
+			_putchar('\n');
+
+
+
+}
