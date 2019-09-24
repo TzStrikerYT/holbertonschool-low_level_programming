c0d8f464b4f8e091c5cbb01b8cb2a8bcd32c1f8e
Michael <maicol.asus@gmail.com>
2019-09-24 15:35:40 +0000

    Task 5 for C project

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * print_sign - check if is a sign or other
+ *
+ * @n: is an variable
+ *
+ * Return: 0 if the letter islower.
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	return (0);
+}
