e1266623f70c0e03b00e2449da9c3c4395d290f3
Michael <maicol.asus@gmail.com>
2019-09-24 16:07:03 +0000

    Task 7 for C project

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * print_last_digit print the last digit
+ *
+ * c is a in variable
+ *
+ * Return: 0 if the letter islower.
+ */
+
+int print_last_digit(int n)
+{
+        int res = n % 10;
+        return (res);
+}
