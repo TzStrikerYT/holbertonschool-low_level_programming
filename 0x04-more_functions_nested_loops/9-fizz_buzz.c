4777c80687442bbe69b5b02f7f69d1ba09d03a4f
Striker <maicol.asus@gmail.com>
2019-09-27 04:25:31 +0000

    task 9 for c project loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,33 +8,33 @@
 
 int main(void)
 {
-        int i = 0;
+	int i = 0;
 
-        for (i = 1; i <= 100; i++)
-        {
-                if (i % 3 == 0 && i % 5 == 0)
-                {
-                        printf("FizzBuzz");
-                }
-                else if (i % 3 == 0)
-                {
-                        printf("Fizz");
-                }
-                else if (i % 5 == 0)
-                {
-                        printf("Buzz");
-                }
-                else
-                {
-                        printf("%d", i);
-                }
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
 
-                if (i != 100)
-                {
-                        printf(" ");
-                }
-        }
-        putchar('\n');
+		if (i != 100)
+		{
+			printf(" ");
+		}
+	}
+	putchar('\n');
 
-        return (0);
+	return (0);
 }
