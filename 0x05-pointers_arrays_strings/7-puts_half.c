19fbdac9f20d5b85dc2460570942cecb596bf150
Michael <maicol.asus@gmail.com>
2019-10-01 19:34:03 +0000

    Task 7 for c project, arrays and pointers

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * puts_half - print half string
+ *
+ * @str: String value
+ *
+ * Return: void variable.
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	int j;
+
+	for (i = 0; *(str + i) != 0; i++)
+	{
+	}
+
+	if (i % 2 != 0)
+	{
+		i -= 1;
+	}
+
+	for (j = i / 2; j <= i; j++)
+	{
+		putchar(*(str + j));
+	}
+	_putchar('\n');
+}
