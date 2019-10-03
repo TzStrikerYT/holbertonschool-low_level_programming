bedf079ebdbd0b7a94a08857f2ffae758cec55a0
Michael <maicol.asus@gmail.com>
2019-10-03 20:13:54 +0000

    Task 1 for Strings project in c

---
+++ 0x06-pointers_arrays_strings/1.strncat.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * *_strncat - concat two Strings.
+ *
+ * @dest: have a 1st String.
+ * @src: have a 2nd String.
+ * @n: integer value.
+ *
+ * Return: concatened String.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int counter = 0;
+
+	for (i = 0; dest[i]; i++)
+	{
+		counter++;
+	}
+
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+	{
+		dest[counter + i] = src[i];
+		dest[counter + i] = '\0';
+	}
+
+	return (dest);
+}
