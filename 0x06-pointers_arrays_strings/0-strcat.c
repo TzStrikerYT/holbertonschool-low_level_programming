934321fc250628a1800476ebae93542e66cf6f9c
Michael <maicol.asus@gmail.com>
2019-10-03 18:36:08 +0000

    Task 0 for c String project

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * *_strcat - concat two Strings.
+ *
+ * @dest: have a 1st String.
+ * @src: have a 2nd String
+ *
+ * Return: concatened String.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (dest[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[j] = '\0';
+
+	return (dest);
+}
