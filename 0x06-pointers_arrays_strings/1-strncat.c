4d17b3808d8749c34e69694529ed3081bc7f8afd
Michael <maicol.asus@gmail.com>
2019-10-03 20:20:19 +0000

    Correction for Task 1 for Strings project in c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -23,8 +23,9 @@ char *_strncat(char *dest, char *src, int n)
 	for (i = 0 ; i < n && src[i] != '\0' ; i++)
 	{
 		dest[counter + i] = src[i];
-		dest[counter + i] = '\0';
+		
 	}
+	dest[counter + i] = '\0';
 
 	return (dest);
 }
