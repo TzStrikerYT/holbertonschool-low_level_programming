abfb72b52bd19beacb01da48ef25283a9dfc7d0f
Michael <maicol.asus@gmail.com>
2019-10-07 17:20:24 +0000

    More pointer, more strings and more arrays, first update of the day

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -1,3 +1,5 @@
+void simple_print_buffer(char *buffer, unsigned int size);
+void simple_print_buffer(char *buffer, unsigned int size);
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
