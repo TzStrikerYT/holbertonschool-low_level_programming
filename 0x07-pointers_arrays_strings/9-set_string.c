ae03c59af521f5991b0d5c99193313e443621b97
Michael <maicol.asus@gmail.com>
2019-10-08 19:41:32 +0000

    Excersice 8 PLD

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * set_string - replace second string to the first string.
+ *
+ * @s: is a poiter to pointer.
+ * @to: is pointer only :).
+ * Return: void variable
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -1,3 +1,4 @@
+void set_string(char **s, char *to);
 void print_chessboard(char (*a)[8]);
 char *_strstr(char *haystack, char *needle);
 char *_strpbrk(char *s, char *accept);
