32e33f4c8b31b2e56464c83357f3005be6d1d227
Michael <maicol.asus@gmail.com>
2019-10-09 19:58:23 +0000

    Task 4, works pow in this project?

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * _pow_recursion - return pow any number.
+ * @x:contains a number.
+ * @y:contains a pow
+ * Return: pow of a number.
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 0 || y < 0)
+	{
+		return (-1);
+	}
+	if (y % 2 == 0)
+	{
+		return (pow(_pow_recursion(x, y / 2), 2));
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
