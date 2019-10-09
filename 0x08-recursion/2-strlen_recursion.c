fb8a37669b4581c4502e4f019b8ff51ac4916525
Michael <maicol.asus@gmail.com>
2019-10-09 18:41:53 +0000

    Task 2 for 0x08 project

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - prints length of any string.
+ * @s: Contains a string to reverse
+ * Return: length of String.
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
