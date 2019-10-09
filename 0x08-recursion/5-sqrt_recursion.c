4e26c6fae557831d17d17d31a741f675c52fbdbe
Michael <maicol.asus@gmail.com>
2019-10-09 22:14:42 +0000

    task 5 for project 0x08 c project

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * _number - help to find raiz nuber.
+ *
+ * @a: number to find raiz.
+ * @x: raiz number.
+ *
+ * Return: raiz of the number.
+ */
+
+int _number(int a, int x)
+{
+	if ((x * x) == a)
+	{
+		return (x);
+	}
+	else if ((x * x) > a)
+	{
+		return (-1);
+	}
+
+	return (_number(a, x + 1));
+}
+
+int _sqrt_recursion(int n)
+{
+	int a;
+
+	a = _number(n, 1);
+	return (n);
+}
