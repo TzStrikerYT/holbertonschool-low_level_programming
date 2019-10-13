52e683c904ec46e205d71067cba43cc8c0b2ca59
root <root@localhost.localdomain>
2019-10-13 19:09:19 -0400

    test new kali linux

--- 0x0A-argc-argv/holberton.h
+++ 0x0A-argc-argv/holberton.h
@@ -1,1 +1,12 @@
+int _vs(char *a, int len);
+int _length(char a, int len);
+int is_palindrome(char *s);
+int _number(int a, int x);
+int is_prime_number(int n);
+int _sqrt_recursion(int n);
+int _pow_recursion(int x, int y);
+int factorial(int n);
+int _strlen_recursion(char *s);
+void _print_rev_recursion(char *s);
 int _putchar(char c);
+void _puts_recursion(char *s);
