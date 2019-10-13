d14f9ac3ec9ecd2c19817f35fdfbb9ba1e6ed860
root <root@localhost.localdomain>
2019-10-13 19:33:50 -0400

    task 3 for c project 0x0a

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,52 +8,19 @@
  * @argv: numbers to multiply
  * Return: Always (0) Success.
  */
-int _check(char *s);
-int _check(char *s)
+
+int main(int argc, char *argv[])
 {
-	int res = 0;
-	int i = 0;
+	(void)argc;
 
-	for (i = 0; *s; i++)
+	if (argv[1] && argv[2])
 	{
-		if (s[i] < 47 || s[i] > 58)
-		{
-			res = 0;
-		}
-		else
-		{
-			res = 1;
-			break;
-		}
+		printf("%d \n", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
 	}
-		return (res);
-}
-
-
-int main (int argc, char *argv[])
-{
-	int check = 0;
-	int i = 0;
-	int j = 0;
-	int sum = 0;
-
-	for (i = 0; i < argc; i++)
+	else
 	{
-		check = _check(argv[i]);
-
-		if (check == 1)
-		{
-			puts("Error");
-			return (1);
-		}
+		printf("Error \n");
+		return (1);
 	}
-
-	for (j = 1; j < argc; j++)
-	{
-		sum += atoi(argv[j]);
-		printf("%d\n", sum);
-	}
-
-	printf("%d\n", sum);
-	return (0);
 }
