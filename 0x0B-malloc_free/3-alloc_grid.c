14558eaa81429948b6667b2eaecf387933291e6a
root <root@localhost.localdomain>
2019-10-15 22:38:48 -0400

    task 3 malloc project for c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,3 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * **alloc_grid - build a matrix
+ * @width: width matrix
+ * @height: heigth matrix
+ *
+ * Return: matrix.
+ */
+
 int **alloc_grid(int width, int height)
 {
 	int x;
@@ -5,32 +17,31 @@ int **alloc_grid(int width, int height)
 	int i;
 	int f = 0;
 	int **mat;
-    
+
 	if (width <= 0 || height <= 0)
 		return (0);
-	
 	mat = malloc(height * sizeof(int *));
-	
+
 	if (mat == 0)
-		return 0;
-	    
+		return (0);
+
 	for (i = 0; i < height; i++)
 	{
 		mat[i] = malloc(width * sizeof(int));
-		        
+
 		if (mat[i] == 0)
 		{
 			f = 1;
 			break;
 		}
-		        
+
 		for (j = 0; j < width; j++)
 		{
 			mat[i][j] = 0;
 		}
-		        
+
 	}
-	    
+
 	if (f == 1)
 	{
 		for (x = 0; x <= i; x++)
--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -1,3 +1,4 @@
+char *create_array(unsigned int size, char c);
 char *str_concat(char *s1, char *s2);
 char *_strdup(char *str);
 char *create_array(unsigned int size, char c);
