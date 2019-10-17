f8f65886cf93a17a7af7386997c08cfa6d22eb96
root <root@localhost.localdomain>
2019-10-17 15:11:44 -0400

    task tree for c project about malloc D:

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * *array_range - Write a function that creates an array of integers.
+ * @min: integer value
+ * @max: integer value
+ * Return: new array of integers.
+ */
+
+int *array_range(int min, int max)
+{
+	int i, size, n;
+	int *a;
+
+	if (min > max)
+		return (NULL);
+
+	size = max - min;
+	a = malloc((size + 1) * sizeof(int));
+	n = min;
+
+	for (i = 0; n <= max; i++, n++)
+		a[i] = n;
+
+	return (a);
+}
