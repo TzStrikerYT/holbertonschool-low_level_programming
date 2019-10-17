7c0a8c0e1486f2dd4eeb512b627675ab49b3d330
root <root@localhost.localdomain>
2019-10-17 14:19:33 -0400

    task 1 for malloc in c project 0x0c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 /**
- * *str_concat - concat two strings.
+ * *string_nconcat - concat two strings.
  * @s1: Contains a string
  * @s2: contains a string to concat whit s1.
  * @n: integer value to count n characters to use in variable s2
@@ -12,8 +12,8 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int len;
-	int len2;
+	unsigned int len;
+	unsigned int len2;
 	unsigned int i;
 	int j;
 	char *a;
@@ -30,7 +30,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (len2 = 0; s2[len2]; len2++)
 		;
 
-	a = malloc(sizeof(char) * (len + len2 + 1));
+	if (n >= len2)
+		n = len2;
+
+	a = malloc(sizeof(char) * (len + n + 1));
 
 	if (a == NULL)
 		return (NULL);
@@ -38,7 +41,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; i < len; i++)
 		a[i] = s1[i];
 
-	for (i = 0, j = len; i <= n; j++, i++)
+	for (i = 0, j = len; i < n; j++, i++)
 		a[j] = s2[i];
 
 	return (a);
