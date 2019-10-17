0a4253211f2ec762c394bcb6b4a7e33d4bb22895
root <root@localhost.localdomain>
2019-10-17 14:28:09 -0400

    task 1 for malloc in c project 0x0c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -41,7 +41,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; i < len; i++)
 		a[i] = s1[i];
 
-	for (i = 0, j = len; i < n; j++, i++)
+	for (i = 0, j = len; s2[j] != '\0' && i < n; j++, i++)
 		a[j] = s2[i];
 
 	a[i] = '\0';
