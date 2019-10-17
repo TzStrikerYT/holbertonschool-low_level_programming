749de2ae22b12f5536cca264d8ed4afe095f81f1
TzStrikerYT <maicol.asus@gmail.com>
2019-10-17 15:23:46 -0400

    okay...

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -3,3 +3,5 @@ int _putchar(int c);
 void *_calloc(unsigned int nmemb, unsigned int size);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *malloc_checked(unsigned int b);
+
+/*Holberton.h*/
