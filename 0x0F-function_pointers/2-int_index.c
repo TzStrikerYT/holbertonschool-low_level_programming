938f8733bde5502d82abede8b34bcc2910bfbf32
Michael <maicol.asus@gmail.com>
2019-10-23 18:30:51 +0000

    task 2 for pointers to functions

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,7 +13,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (size <= 0)
 		return (-1);
 
-	if (!array || !action)
+	if (!array || !cmp)
 		return (-1);
 
 	for (i = 0; i < size; i++)
