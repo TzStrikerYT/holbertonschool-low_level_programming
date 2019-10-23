7f3b980dff2e70b38febbef2fcfcc45926d70a4c
Michael <maicol.asus@gmail.com>
2019-10-23 23:36:02 +0000

    fix more fix

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -52,7 +52,7 @@ int op_div(int a, int b)
 	if (b == 0)
 	{
 		printf("Error\n");
-		exit(99);
+		exit(100);
 	}
 
 	return (a / b);
@@ -71,7 +71,7 @@ int op_mod(int a, int b)
 	if (b == 0)
 	{
 		printf("Error\n");
-		exit(99);
+		exit(100);
 	}
 
 	return (a % b);
