01370979fd4e43b3afe45612ca18ff36e7a250b4
Michael <maicol.asus@gmail.com>
2019-10-23 17:58:04 +0000

    task 1 for pointers to functions project

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+/**
+ * array_iterator - executes any function.
+ * @array: contains an array of integers.
+ * @size: contains a size of the array.
+ * @action: contains a function to execute.
+ * @void function.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
