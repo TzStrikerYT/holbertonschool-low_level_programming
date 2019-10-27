b602ad0ada22a6b03f3218e874f18e7d16bf1343
Striker <maicol.asus@gmail.com>
2019-10-27 20:11:08 +0000

    Fix header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,8 +7,13 @@
 #include <stdarg.h>
 
 int _putchar(char);
+void print_numbers(const char *separator, const unsigned int n, ...);
 int sum_them_all(const unsigned int, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
 #endif /* VARIADIC_FUNCTIONS */
+
+#ifndef STRUCTS
+#define STRUCTS
+#endif
