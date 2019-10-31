673bd070550f51e4a6cfc82191f480923446d762
Michael <maicol.asus@gmail.com>
2019-10-31 19:47:07 +0000

    [fix] fix error for checkers

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -22,37 +22,6 @@ int _strlen(char *s)
 }
 
 /**
- * *_strdup - copy an array.
- * @str: Contains a string to copy..
- *
- * Return: new array.
- */
-
-char *_strdup(const char *str)
-{
-	char *a;
-	int i;
-	int j;
-
-	if (str == NULL)
-		return (NULL);
-
-	for (i = 0; str[i]; i++)
-	{
-	}
-
-	a = malloc(sizeof(char) * i + 1);
-
-	if (a == NULL)
-		return (NULL);
-
-	for (j = 0; j < i; j++)
-		a[j] = str[j];
-
-	return (a);
-}
-
-/**
  * add_node - add a new node in a list
  * @head: pointer to linked list.
  * @str: Contains a string to add.
@@ -64,12 +33,12 @@ list_t *add_node(list_t **head, const char *str)
 	char *temp;
 	list_t *node = malloc(sizeof(list_t));
 
-	if (str == NULL)
-		return(0);
+	if (node == NULL)
+		return (0);
 
 	else
 	{
-		temp = _strdup(str);
+		temp = strdup(str);
 
 		if (temp == 0)
 			return (0);
