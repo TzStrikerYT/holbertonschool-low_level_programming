7e71615b9d93b5ab2662f8ba94d72177d909a02e
Michael <maicol.asus@gmail.com>
2019-10-31 18:53:32 +0000

    [add] addiing task 2 for 0x12 project

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,16 +1,33 @@
 #include "lists.h"
 
 /**
- * list_len - Return number of elements into the list.
- * @h: Call structure in the header file.
- * Return: Number of elements
+ * add_node - add a new node in a list
+ * @head: pointer to linked list.
+ * @str: Contains a string to add.
+ * Return: address of a new element.
  */
 
 list_t *add_node(list_t **head, const char *str)
 {
 	char *temp;
-	list_t node = malloc(sizeof(list_t);
+	list_t *node = malloc(sizeof(list_t));
 
-			     if (node == null
+	if (str == NULL)
+	{
+		node->str = 0;
+		node->len = 0;
+	}
+	else
+	{
+		temp = strdup(str);
 
+		if (temp == 0)
+			return (0);
+		node->str = temp;
+		node->len = strlen(temp);
+	}
+
+	node->next = *head;
+	*head = node;
+	return (node);
 }
