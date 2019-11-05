68402c70e01274e8c74ff5bae3bd4e9bc1c25a8c
Michael <maicol.asus@gmail.com>
2019-11-05 19:21:51 +0000

    [fix] fix file for task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,11 +12,12 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 
-	if (head == 0)
+	for (i = 0; i < index; i++)
+	{
+		if (head == 0)
 		return (0);
 
-	for (i = 0; i < index; i++)
 		head = head->next;
-
+	}
 	return (head);
 }
