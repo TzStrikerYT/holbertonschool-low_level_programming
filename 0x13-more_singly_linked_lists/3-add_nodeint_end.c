4c8509f7976d9ac55256412c3dc6444a84c3e9ff
Michael <maicol.asus@gmail.com>
2019-11-05 16:16:55 +0000

    [fix] fix type error

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,14 +9,14 @@
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	list_t *tmp;
-	list_t *node = malloc(sizeof(list_t));
+	listint_t *tmp;
+	listint_t *node = malloc(sizeof(listint_t));
 
 	if (node == 0)
 		return (0);
 
 	tmp = *head;
-	node->n = strdup(n);
+	node->n = n;
 	node->next = 0;
 
 	if (!tmp)
