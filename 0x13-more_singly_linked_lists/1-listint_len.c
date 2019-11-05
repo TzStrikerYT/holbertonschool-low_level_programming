d01936d7c26763b84a303e943dc7fe5572715050
Michael <maicol.asus@gmail.com>
2019-11-05 15:49:22 +0000

    [add] add task 1 for c project 0x13

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * listint_len - Return number of elements into the list.
+ * @h: Call structure in the header file.
+ * Return: Number of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	unsigned int s;
+
+	for (s = 0; h != 0; size++)
+		h = h->next;
+
+	return (size);
+}
