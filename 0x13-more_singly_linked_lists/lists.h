cd476358e95f21a3e0e2c1377403465edd6b0271
Michael <maicol.asus@gmail.com>
2019-11-05 16:56:05 +0000

    [add] add files for project 0x13 task 2

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -25,6 +25,7 @@ typedef struct listint_s
 #include <stdlib.h>
 #include <string.h>
 
+void free_listint(listint_t *head);
 listint_t *add_nodeint(listint_t **head, const int n);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
