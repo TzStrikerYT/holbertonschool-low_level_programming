fcc65ed0529a3ba27962679d20237aad6d797a10
Striker <maicol.asus@gmail.com>
2019-11-08 01:14:03 +0000

    [fix] fix betty style task 5 for c project

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -14,7 +14,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	int dist = 0;
 
 	while (sh--)
-	      dist += (n >> sh & 1) != (m >> sh & 1);
+		dist += (n >> sh & 1) != (m >> sh & 1);
 
-	      return (dist);
+	return (dist);
 }
