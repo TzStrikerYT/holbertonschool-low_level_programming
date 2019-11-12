db53be059f4cb5c4c5eacb6fc1eaa1d883d56adb
Michael <maicol.asus@gmail.com>
2019-11-12 15:45:50 +0000

    add firsts files

---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,20 @@
+#ifndef HEADERS
+#define HEADERS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#endif /* HEADERS */
+
+#ifndef PROTOTYPE_FUNCTIONS
+#define PROTOTYPE_FUNCTIONS
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif /* PROTOTYPE_FUNCTIONS */
