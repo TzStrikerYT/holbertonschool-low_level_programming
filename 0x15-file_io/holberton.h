850841fc8401b868d8f2c5b8a61f0e152e6f30e0
TzStrikerYT <maicol.asus@gmail.com>
2019-11-13 01:24:50 +0000

    [mod] modify header file for works whit a cp function

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -18,3 +18,14 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
 #endif /* PROTOTYPE_FUNCTIONS */
+
+#ifndef MACROS_FOR_CP
+#define MACROS_FOR_CP
+
+#define BUFFER_SIZE 1024
+#define USAGE "Usage: cp file_from file_to\n"
+#define ERROR_READ "Error: Can't read from file %s\n"
+#define ERROR_WRITE "Error: Can't write to %s\n"
+#define ERROR_CLOSE "Error: Can't close fd %d\n"
+
+#endif /* MACROS_FOR_CP */
