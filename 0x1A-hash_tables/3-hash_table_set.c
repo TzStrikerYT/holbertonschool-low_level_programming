8abc94bd64f799262a7b179e1488046ae0158053
TzStrikerYT <maicol.asus@gmail.com>
2020-01-19 00:26:07 -0500

    [add] task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,72 @@
+#include "hash_tables.h"
+
+/**
+ * add_hash_node - adds hash node.
+ * @key: key you want add.
+ * @value:value of the key.
+ * Return: new node, NULL on error.
+ */
+
+hash_node_t *add_hn(const char *key, const char *value)
+{
+	hash_node_t *n_node;
+
+	n_node = malloc(sizeof(hash_node_t));
+	if (!n_node)
+		return (NULL);
+
+	n_node->key = strdup(key);
+	n_node->value = strdup(value);
+
+	if (!n_node->key || !n_node->value)
+	{
+		if (n_node->key)
+			free(n_node->key);
+		free(n_node);
+		return (NULL);
+	}
+
+	return (n_node);
+}
+
+/**
+ * hash_table_set - adds an element to the hash table.
+ * @ht: the hash table.
+ * @key: key you want add.
+ * @value:value of the key.
+ * Return: new node, NULL on error.
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *node, *n_node, *tmp;
+
+	if (!ht || !key || !value || !strlen(key) == 0 || !ht->array)
+		return (0);
+
+	index = key_index((unsigned char *)key, ht->size);
+	tmp = ht->array[index];
+	node = tmp;
+
+	if (node)
+	{
+		while (tmp)
+		{
+			if (strcmp(tmp->key, key) == 1)
+			{
+				free(tmp->value);
+				tmp->value = strdup(value);
+				return (1);
+			}
+			tmp = tmp->next;
+		}
+	}
+
+	n_node = add_hn(key, value);
+	if (!n_node)
+		return (0);
+	n_node->next = node;
+	ht->array[index] = n_node;
+	return (1);
+}
