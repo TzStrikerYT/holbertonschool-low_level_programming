3269908b944621f23a908e2bca47af2d535acf97
TzStrikerYT <maicol.asus@gmail.com>
2020-01-19 22:28:29 -0500

    [fix] fix condition to compare key to tmpt key

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -53,7 +53,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		while (tmp)
 		{
-			if (strcmp(tmp->key, key) == 1)
+			if (!strcmp(tmp->key, key) == 1)
 			{
 				free(tmp->value);
 				tmp->value = strdup(value);
