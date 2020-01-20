c592c78749ad988d80206d2de92e965562dbd40a
TzStrikerYT <maicol.asus@gmail.com>
2020-01-19 21:55:48 -0500

    [add] adding task 4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a hash value
+ * @ht: hast table
+ * @key: Number of key
+ * Return: The value requested
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	if (!ht || !key)
+		return (NULL);
+
+	index = key_index((unsigned char *)key, ht->size);
+
+	if (!ht->array || !(ht->array)[index])
+		return (NULL);
+
+	node = (ht->array)[index];
+
+	while(strcmp(node->key, key))
+		node = node->next;
+
+	return (node ? node->value : NULL);
+}
