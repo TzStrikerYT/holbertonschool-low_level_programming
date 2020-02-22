6c792df880bdcba04dff6d2cc73dec515fecbccc
TzStrikerYT <maicol.asus@gmail.com>
2020-02-21 23:20:01 -0500

    [fix] fix. incorrect operation in the if for y position

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,7 +9,7 @@ def island_perimeter(grid):
     for y, r in enumerate(grid):
         for x, c in enumerate(r):
             if c == 1:
-                if y == 0 or grid[y + 1][x] == 0:
+                if y == 0 or grid[y - 1][x] == 0:
                     perimeter += 1
                 if x == 0 or grid[y][x - 1] == 0:
                     perimeter += 1
