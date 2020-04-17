a34efb78a5f5edb60e25377c3eb2241cca716d68
Michael <maicol.asus@gmail.com>
2020-04-17 04:39:51 +0000

    [add] add task1

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,42 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for a value in an array using binary search
+ * @array: input array
+ * @size: size of the array
+ * @value: the value to search
+ *
+ * Return: index of the value founded
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	int a = 0;
+	int b = (int)size - 1;
+	int i;
+
+	for (i = 0; a <= b;)
+	{
+		printf("Sarching in array: ");
+		p_array(array, a, b);
+		i = (a + b) / 2;
+
+		if (array[i] < value)
+			a = i + 1;
+		else if (array[i] > value)
+			b = i - 1;
+		else
+			return (i);
+	}
+	return (-1);
+}
+
+void p_array(int *array, int a, int b)
+{
+	int i;
+
+	for (i = a; i < b; i++)
+		printf("%d, ", array[i]);
+
+	printf("%d\n", array[i]);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,5 +4,6 @@
 #include <stdlib.h>
 
 int linear_search(int *array, size_t size, int value);
-
+int binary_search(int *array, size_t size, int value);
+void p_array(int *array, int a, int b);
 #endif /* S_ALGOS */
